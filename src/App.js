49ff7e54af64a483d648a7e68e3e54cafdc37edc
Austinrainy <rainy-boy-today@126.com>
2019-04-21 16:27:25 +0800

    多次setState

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+        val:0,
+    }
+}
+  componentDidMount(){
+    this.setState({val:this.state.val+1});
+    console.log(this.state.val);
+   
+    setTimeout(()=>{
+    this.setState({val:this.state.val+1});
+    console.log(this.state.val);
+  },3000);
+    setTimeout(()=>{
+      this.setState({val:this.state.val+1});
+      console.log(this.state.val);
+    },100)
+    
+    this.setState({val:this.state.val+1});
+    console.log(this.state.val);
+}
   render() {
+    const {val} =this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +34,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>{val}</div>
           <a
             className="App-link"
             href="https://reactjs.org"
